f10bace3e8a1d7dc06dac924af82ea48731233cb
MichalFrends1 <michal.nowakowski@frends.com>
2026-01-21 09:20:31 +0100

    fux docs

--- Frends.PDF.Read/Frends.PDF.Read/Definitions/Error.cs
+++ Frends.PDF.Read/Frends.PDF.Read/Definitions/Error.cs
@@ -10,7 +10,7 @@ public class Error
     /// <summary>
     /// Summary of the error.
     /// </summary>
-    /// <example>Unable read file.</example>
+    /// <example>Unable to read PDF file.</example>
     public string Message { get; set; }
 
     /// <summary>
